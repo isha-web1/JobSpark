b8041a1b6dcbd4321b1e81a7afd2f0a0c0cf5bcf
isha-web1 <isha.web1@gmail.com>
2025-03-22 23:27:36 +0600

    initial setup done

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            {/* nav bar */}
+             <h1>main page</h1>
+            {/* outLate */}
+
+            {/* footer */}
+        </div>
+    );
+};
+
+export default Main;
