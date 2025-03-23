683e8e1f0d46857db6ede17877c53e4755740391
isha-web1 <isha.web1@gmail.com>
2025-03-23 23:56:54 +0600

    jobDetails & addJob components added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,16 +7,16 @@ const Navbar = () => {
     return (
       <div className='navbar bg-base-100 shadow-sm container px-4 mx-auto'>
         <div className='flex-1'>
-          <div className='flex gap-1 items-center '>
+          <Link to='/' className='flex gap-1 items-center '>
             <p className="w-auto"><FaUserTie /></p>
             <span className='font-bold'>Job<span className="text-cyan-400">Spark</span> </span>
-          </div>
+          </Link>
         </div>
         <div className='flex-none'>
           <ul className='menu menu-horizontal px-1'>
-            <li>
+            <Link to='/'>
               <div>Home</div>
-            </li>
+            </Link>
   
             {!user && (
             <li>
@@ -43,17 +43,19 @@ const Navbar = () => {
               tabIndex={0}
               className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
             >
-              <li>
-                <div className='justify-between'>Add Job</div>
+               <li>
+                <Link to='/add-job' className='justify-between'>
+                  Add Job
+                </Link>
               </li>
               <li>
-                <div>My Posted Jobs</div>
+                <Link to='/my-posted-jobs'>My Posted Jobs</Link>
               </li>
               <li>
-                <div>My Bids</div>
+                <Link to='/my-bids'>My Bids</Link>
               </li>
               <li>
-                <div>Bid Requests</div>
+                <Link to='/bid-requests'>Bid Requests</Link>
               </li>
               <li className='mt-2'>
               <button
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,26 +3,38 @@ import Main from "../layouts/Main";
 import Home from "../pages/Home";
 import Login from "../pages/Authentication/Login";
 import Register from "../pages/Authentication/Register";
+import JobDetails from "../pages/JobDetails";
+import AddJobs from "../pages/AddJobs";
 
 const router = createBrowserRouter([
-    {
-        path : '/',
-        element : <Main/>,
-        children : [
-            {
-                index : true,
-                element : <Home/>
-            },
-            {
-                path : '/login',
-                element : <Login/>
-            },
-            {
-                path : '/register',
-                element : <Register/>
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/job/:id",
+        element: <JobDetails />,
+        loader: ({ params }) =>
+          fetch(`${import.meta.env.VITE_API_URL}/job/${params.id}`),
+      },
+      {
+        path: '/add-job',
+        element: <AddJobs/>,
+      },
+    ],
+  },
+]);
 
 export default router;
