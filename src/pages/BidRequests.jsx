a6b0a1ed835feb5965f63a267c45d10a298d9574
isha-web1 <isha.web1@gmail.com>
2025-04-08 20:08:07 +0600

    handle bid status

--- src/pages/BidRequests.jsx
+++ src/pages/BidRequests.jsx
@@ -15,110 +15,157 @@ const BidRequests = () => {
       const {data} = await axios(`${import.meta.env.VITE_API_URL}/bid-requests/${user?.email}`)
       setBids(data)
     }
+
+    // handleStatus
+  const handleStatus = async (id, prevStatus, status) => {
+    console.log(id, prevStatus, status)
+    if (prevStatus === status) return console.log('Sry vai.. hobena')
+    await mutateAsync({ id, status })
+  }
+
+  // if (isLoading) return <p>Data is still loading......</p>
     return (
       <section className='container px-4 mx-auto pt-12'>
-        <div className='flex items-center gap-x-3'>
-          <h2 className='text-lg font-medium text-gray-800 '>Bid Requests</h2>
-  
-          <span className='px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full '>
-            {bids.length} Requests
-          </span>
-        </div>
-  
-        <div className='flex flex-col mt-6'>
-          <div className='-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
-            <div className='inline-block min-w-full py-2 align-middle md:px-6 lg:px-8'>
-              <div className='overflow-hidden border border-gray-200  md:rounded-lg'>
-                <table className='min-w-full divide-y divide-gray-200'>
-                  <thead className='bg-gray-50'>
-                    <tr>
-                      <th
-                        scope='col'
-                        className='py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500'
-                      >
-                        <div className='flex items-center gap-x-3'>
-                          <span>Title</span>
-                        </div>
-                      </th>
-                      <th
-                        scope='col'
-                        className='py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500'
-                      >
-                        <div className='flex items-center gap-x-3'>
-                          <span>Email</span>
-                        </div>
-                      </th>
-  
-                      <th
-                        scope='col'
-                        className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
-                      >
-                        <span>Deadline</span>
-                      </th>
-  
-                      <th
-                        scope='col'
-                        className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
-                      >
-                        <button className='flex items-center gap-x-2'>
-                          <span>Price</span>
-                        </button>
-                      </th>
-  
-                      <th
-                        scope='col'
-                        className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
-                      >
-                        Category
-                      </th>
-  
-                      <th
-                        scope='col'
-                        className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
-                      >
-                        Status
-                      </th>
-  
-                      <th className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'>
-                        Actions
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className='bg-white divide-y divide-gray-200 '>
-                    <tr>
+      <div className='flex items-center gap-x-3'>
+        <h2 className='text-lg font-medium text-gray-800 '>Bid Requests</h2>
+
+        <span className='px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full '>
+          {bids.length} Requests
+        </span>
+      </div>
+
+      <div className='flex flex-col mt-6'>
+        <div className='-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+          <div className='inline-block min-w-full py-2 align-middle md:px-6 lg:px-8'>
+            <div className='overflow-hidden border border-gray-200  md:rounded-lg'>
+              <table className='min-w-full divide-y divide-gray-200'>
+                <thead className='bg-gray-50'>
+                  <tr>
+                    <th
+                      scope='col'
+                      className='py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500'
+                    >
+                      <div className='flex items-center gap-x-3'>
+                        <span>Title</span>
+                      </div>
+                    </th>
+                    <th
+                      scope='col'
+                      className='py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500'
+                    >
+                      <div className='flex items-center gap-x-3'>
+                        <span>Email</span>
+                      </div>
+                    </th>
+
+                    <th
+                      scope='col'
+                      className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
+                    >
+                      <span>Deadline</span>
+                    </th>
+
+                    <th
+                      scope='col'
+                      className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
+                    >
+                      <button className='flex items-center gap-x-2'>
+                        <span>Price</span>
+                      </button>
+                    </th>
+
+                    <th
+                      scope='col'
+                      className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
+                    >
+                      Category
+                    </th>
+
+                    <th
+                      scope='col'
+                      className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'
+                    >
+                      Status
+                    </th>
+
+                    <th className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500'>
+                      Actions
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className='bg-white divide-y divide-gray-200 '>
+                  {bids.map(bid => (
+                    <tr key={bid._id}>
                       <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
-                        Build Dynamic Website
+                        {bid.job_title}
                       </td>
                       <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
-                        example@gmail.com
+                        {bid.email}
                       </td>
-  
+
                       <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
-                        10/04/2024
+                        {new Date(bid.deadline).toLocaleDateString()}
                       </td>
-  
+
                       <td className='px-4 py-4 text-sm text-gray-500  whitespace-nowrap'>
-                        $200
+                        ${bid.price}
                       </td>
                       <td className='px-4 py-4 text-sm whitespace-nowrap'>
                         <div className='flex items-center gap-x-2'>
                           <p
-                            className='px-3 py-1 rounded-full text-blue-500 bg-blue-100/60
-                             text-xs'
+                            className={`px-3 py-1 rounded-full ${
+                              bid.category === 'Web Development' &&
+                              'text-blue-500 bg-blue-100/60'
+                            } ${
+                              bid.category === 'Graphics Design' &&
+                              'text-emerald-500 bg-emerald-100/60'
+                            } ${
+                              bid.category === 'Digital Marketing' &&
+                              'text-pink-500 bg-pink-100/60'
+                            } text-xs`}
                           >
-                            Web Development
+                            {bid.category}
                           </p>
                         </div>
                       </td>
                       <td className='px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap'>
-                        <div className='inline-flex items-center px-3 py-1 rounded-full gap-x-2 bg-yellow-100/60 text-yellow-500'>
-                          <span className='h-1.5 w-1.5 rounded-full bg-yellow-500'></span>
-                          <h2 className='text-sm font-normal '>Pending</h2>
+                        <div
+                          className={`inline-flex items-center px-3 py-1 rounded-full gap-x-2 ${
+                            bid.status === 'Pending' &&
+                            'bg-yellow-100/60 text-yellow-500'
+                          } ${
+                            bid.status === 'In Progress' &&
+                            'bg-blue-100/60 text-blue-500'
+                          } ${
+                            bid.status === 'Complete' &&
+                            'bg-emerald-100/60 text-emerald-500'
+                          } ${
+                            bid.status === 'Rejected' &&
+                            'bg-red-100/60 text-red-500'
+                          } `}
+                        >
+                          <span
+                            className={`h-1.5 w-1.5 rounded-full ${
+                              bid.status === 'Pending' && 'bg-yellow-500'
+                            } ${
+                              bid.status === 'In Progress' && 'bg-blue-500'
+                            } ${bid.status === 'Complete' && 'bg-green-500'} ${
+                              bid.status === 'Rejected' && 'bg-red-500'
+                            }  `}
+                          ></span>
+                          <h2 className='text-sm font-normal '>{bid.status}</h2>
                         </div>
                       </td>
                       <td className='px-4 py-4 text-sm whitespace-nowrap'>
                         <div className='flex items-center gap-x-6'>
-                          <button className='text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none'>
+                          {/* Accept Button: In Progress */}
+                          <button
+                            onClick={() =>
+                              handleStatus(bid._id, bid.status, 'In Progress')
+                            }
+                            disabled={bid.status === 'Complete'}
+                            className='disabled:cursor-not-allowed text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none'
+                          >
                             <svg
                               xmlns='http://www.w3.org/2000/svg'
                               fill='none'
@@ -134,8 +181,14 @@ const BidRequests = () => {
                               />
                             </svg>
                           </button>
-  
-                          <button className='text-gray-500 transition-colors duration-200   hover:text-yellow-500 focus:outline-none'>
+                          {/* Reject Button */}
+                          <button
+                            onClick={() =>
+                              handleStatus(bid._id, bid.status, 'Rejected')
+                            }
+                            disabled={bid.status === 'Complete'}
+                            className='disabled:cursor-not-allowed text-gray-500 transition-colors duration-200   hover:text-yellow-500 focus:outline-none'
+                          >
                             <svg
                               xmlns='http://www.w3.org/2000/svg'
                               fill='none'
@@ -154,13 +207,14 @@ const BidRequests = () => {
                         </div>
                       </td>
                     </tr>
-                  </tbody>
-                </table>
-              </div>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
     )
   }
   
